4cb197a51f6cb1a294ba86b58539b85b49825e8d
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-11-27 20:37:08 -0300

    09  - Manipulando erros

--- 05-Avancando-com-PHP-e-MySQL/utils/connection.php
+++ 05-Avancando-com-PHP-e-MySQL/utils/connection.php
@@ -1,5 +1,20 @@
 <?php
 
+$debug = true;
+if($debug){ // configuração interna do php.ini
+    /*
+     *  MYSQLI_REPORT_ERROR - mostra os erros
+     * MYSQLI_REPORT_OFF - oculta os erros
+     * MYSQLI_REPORT_STRICT - trocar os erros para exceções 
+     * MYSQLI_REPORT_INDEX - informa se há algum index ruim
+     * MYSQLI_REPORT_ALL - todos menos o OFF
+     */
+    mysqli_report(MYSQLI_REPORT_ERROR);
+}else{
+    mysqli_report(MYSQLI_REPORT_OFF);
+}
+
+
 $conn = new mysqli('localhost', 'root', '', 'schoolofnet_avancando-com-php-e-mysql');
 
 if($conn->connect_errno){
